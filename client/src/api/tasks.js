9433b48b75e2e5235a6759161a2c9c1ba811b700
Lyss2120 <lys.ivonne@gmail.com>
2025-08-26 18:53:05 -0400

    second commit{

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -2,14 +2,8 @@ import axios from "axios";
 
 
 
-export const getAllTasks = () => {
-    return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
-}
-// export async function getAllTasks() {
-//   try {
-//     const response = await axios.get('http://127.0.0.1:8000/tasks/api/v1/tasks');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+const  url= axios.create({baseURL: 'http://localhost:8000/tasks/api/v1/tasks/'});
+
+export const getAllTasks = () => url.get('/');
+
+export const createTask = (task) =>  url.post('/', task);
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
+import { createTask } from '../api/tasks'
 
 export const TaskFormPage = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const onSubmit = handleSubmit(async data => {
+        const res = await createTask(data)
+        console.log(res);
+    })
+
     return (
-        <div>TaskFormPage</div>
+        <div className='task-form-wrapper'>
+            <form className='task-form' onSubmit={onSubmit}>
+
+                <input type="text" placeholder='Title'
+                    {...register('title', { required: true })}
+                />  {errors.title && <p>title field is required</p>}
+
+                <textarea rows='3' placeholder="Description" id=""
+                    {...register('description', { required: true })}>
+                </textarea>
+                    {errors.description && <p>description field is required</p>}
+
+                <button>
+                    Save
+                </button>
+                
+            </form>
+        </div>
     )
 }
